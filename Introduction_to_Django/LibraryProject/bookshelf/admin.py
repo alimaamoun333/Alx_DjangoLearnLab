5bb2016c10d7f7d05963ee22d54e31324a6239c6
Ali Maamoun <alimaamoon1@gmail.com>
2025-08-30 01:20:10 +0300

    ...

--- Introduction_to_Django/LibraryProject/bookshelf/admin.py
+++ Introduction_to_Django/LibraryProject/bookshelf/admin.py
@@ -3,6 +3,9 @@ from .models import Book
 
 from django.contrib import admin
 
+
 @admin.register(Book)
 class BookAdmin(admin.ModelAdmin):
-    pass
+    list_display    = ('title', 'author', 'publication_year')
+    list_filter     = ('publication_year',)
+    search_fields   = ('title', 'author')
