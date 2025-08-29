98684a519e44046ee97718f66dda4c4200c60ae6
Ali Maamoun <alimaamoon1@gmail.com>
2025-08-30 01:18:47 +0300

    ..

--- Introduction_to_Django/LibraryProject/bookshelf/admin.py
+++ Introduction_to_Django/LibraryProject/bookshelf/admin.py
@@ -1,9 +1,8 @@
-from django.contrib import admin
+
+from .models import Book
 
 from django.contrib import admin
 
 @admin.register(Book)
 class BookAdmin(admin.ModelAdmin):
     pass
-
-# Register your models here.
