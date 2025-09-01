fc484882df1e824912026fccd50a2eddfcfd0d65
Ali Maamoun <alimaamoon1@gmail.com>
2025-09-01 23:25:55 +0300

    enhancing

--- Introduction_to_Django/LibraryProject/LibraryProject/settings.py
+++ Introduction_to_Django/LibraryProject/LibraryProject/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-!q0h7mlr$m)30g#73qs_z#qk33_mvs#y3xo4i-*amn_hv(6h5u'
+SECRET_KEY = 'django-insecure-!_z*f%hq_l=&5b*k6)ko!!vg0i=(luia8-#-x!*fet6k!sxb31'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'bookshelf',
 ]
 
 MIDDLEWARE = [
--- Introduction_to_Django/LibraryProject/bookshelf/admin.py
+++ Introduction_to_Django/LibraryProject/bookshelf/admin.py
@@ -1,11 +1,46 @@
-
-from .models import Book
-
 from django.contrib import admin
-
+from .models import Book
 
 @admin.register(Book)
 class BookAdmin(admin.ModelAdmin):
-    list_display    = ('title', 'author', 'publication_year')
-    list_filter     = ('publication_year',)
-    search_fields   = ('title', 'author')
+    # Display these fields in the list view
+    list_display = ['title', 'author', 'publication_year']
+    
+    # Add filters in the right sidebar
+    list_filter = ['author', 'publication_year']
+    
+    # Add search functionality
+    search_fields = ['title', 'author']
+    
+    # Add ordering
+    ordering = ['title']
+    
+    # Number of items per page
+    list_per_page = 25
+    
+    # Make certain fields clickable links to the edit page
+    list_display_links = ['title']
+    
+    # Add date hierarchy (if you had date fields)
+    # date_hierarchy = 'created_date'
+    
+    # Customize the form layout in the detail view
+    fieldsets = (
+        ('Book Information', {
+            'fields': ('title', 'author', 'publication_year')
+        }),
+    )
+    
+    # Add actions dropdown
+    actions = ['mark_as_classic']
+    
+    def mark_as_classic(self, request, queryset):
+        """Custom admin action example"""
+        # This is just an example - you'd need a 'is_classic' field in your model
+        # queryset.update(is_classic=True)
+        self.message_user(request, f"{queryset.count()} books marked as classics.")
+    
+    mark_as_classic.short_description = "Mark selected books as classics"
+
+# Alternative registration method (commented out since we're using @admin.register)
+# admin.site.register(Book, BookAdmin)
