b768ba04ed28cef41058a3f3c4b70c61ac4c74d5
Ali Maamoun <alimaamoon1@gmail.com>
2025-09-09 01:59:13 +0300

    pass auto check

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -5,9 +5,9 @@ from django.http import HttpResponseForbidden
 from django.contrib.auth.models import User
 from django.contrib import messages
 from django import forms
+from django.views.generic.detail import DetailView
 from .models import Book, Author, UserProfile, Library
-from .models import Library
-
+from .models import  Library
 
 class BookForm(forms.ModelForm):
     """Form for creating and editing books"""
