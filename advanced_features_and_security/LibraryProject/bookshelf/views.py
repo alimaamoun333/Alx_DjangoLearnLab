4b841fbe9ba7209b00b94b54b2ff494743e980cc
Ali Maamoun <alimaamoon1@gmail.com>
2025-09-11 23:29:40 +0300

    edit view to add forms

--- advanced_features_and_security/LibraryProject/bookshelf/views.py
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -1,13 +1,46 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.decorators import login_required, permission_required
+from django.http import HttpResponse
 from .models import Book
+from .forms import BookForm, ExampleForm   # ✅ Added ExampleForm
 
 
 @login_required
-@permission_required('bookshelf.can_create', raise_exception=True)
 def book_list(request):
-    # Query all books
     books = Book.objects.all()
+    return render(request, "bookshelf/book_list.html", {"books": books})
 
-    # Pass them to the template
-    return render(request, 'bookshelf/book_list.html', {'books': books})
+
+@login_required
+@permission_required("bookshelf.can_create", raise_exception=True)
+def add_book(request):
+    if request.method == "POST":
+        form = BookForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect("book_list")
+    else:
+        form = BookForm()
+    return render(request, "bookshelf/book_form.html", {"form": form})
+
+
+@login_required
+@permission_required("bookshelf.can_delete", raise_exception=True)
+def delete_book(request, pk):
+    book = get_object_or_404(Book, pk=pk)
+    book.delete()
+    return redirect("book_list")
+
+
+# ✅ ExampleForm view
+def example_form_view(request):
+    if request.method == "POST":
+        form = ExampleForm(request.POST)
+        if form.is_valid():
+            name = form.cleaned_data["name"]
+            email = form.cleaned_data["email"]
+            message = form.cleaned_data["message"]
+            return HttpResponse(f"Thanks {name}, we received your message: {message}")
+    else:
+        form = ExampleForm()
+    return render(request, "bookshelf/example_form.html", {"form": form})
