d93047582c4a7c6d51287597cdd0db7ebd8bf404
Ali Maamoun <alimaamoon1@gmail.com>
2025-09-11 21:05:24 +0300

    add CustomUser, CustomUserAdmin to admin

--- advanced_features_and_security/LibraryProject/bookshelf/admin.py
+++ advanced_features_and_security/LibraryProject/bookshelf/admin.py
@@ -2,54 +2,51 @@ from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 from .models import CustomUser, Book
 
-@admin.register(Book)
+
 class BookAdmin(admin.ModelAdmin):
     # Display these fields in the list view
     list_display = ['title', 'author', 'publication_year']
-    
+
     # Add filters in the right sidebar
     list_filter = ['author', 'publication_year']
-    
+
     # Add search functionality
     search_fields = ['title', 'author']
-    
+
     # Add ordering
     ordering = ['title']
-    
+
     # Number of items per page
     list_per_page = 25
-    
+
     # Make certain fields clickable links to the edit page
     list_display_links = ['title']
-    
-    # Add date hierarchy (if you had date fields)
-    # date_hierarchy = 'created_date'
-    
+
     # Customize the form layout in the detail view
     fieldsets = (
         ('Book Information', {
             'fields': ('title', 'author', 'publication_year')
         }),
     )
-    
+
     # Add actions dropdown
     actions = ['mark_as_classic']
-    
+
     def mark_as_classic(self, request, queryset):
         """Custom admin action example"""
         # This is just an example - you'd need a 'is_classic' field in your model
         # queryset.update(is_classic=True)
         self.message_user(request, f"{queryset.count()} books marked as classics.")
-    
+
     mark_as_classic.short_description = "Mark selected books as classics"
 
-@admin.register(CustomUser)
+
 class CustomUserAdmin(UserAdmin):
     fieldsets = UserAdmin.fieldsets + (
         (None, {'fields': ('date_of_birth', 'profile_photo')}),
     )
 
-@admin.register(Book)
-class BookAdmin(admin.ModelAdmin):
-    list_display = ('title', 'author', 'publication_year')
-    search_fields = ('title', 'author')
+
+# Register models explicitly
+admin.site.register(Book, BookAdmin)
+admin.site.register(CustomUser, CustomUserAdmin)
