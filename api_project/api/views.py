77fae6331b0e3fbe9993f9dad3c4ab311aee0a62
Ali Maamoun <alimaamoon1@gmail.com>
2025-09-18 20:59:40 +0300

    .

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -5,3 +5,9 @@ from .serializers import BookSerializer
 class BookViewSet(viewsets.ModelViewSet):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
+
+
+class BookList(generics.ListAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+
