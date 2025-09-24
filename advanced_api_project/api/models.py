9a115852c118bed536fae5f0e7243a32bc6428c6
Ali Maamoun <alimaamoon1@gmail.com>
2025-09-24 21:59:46 +0300

    Serializers in Django REST Framework

--- advanced_api_project/api/models.py
+++ advanced_api_project/api/models.py
@@ -1,3 +1,27 @@
 from django.db import models
 
-# Create your models here.
+class Author(models.Model):
+    """
+    Represents an author who can have multiple books.
+    """
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+
+class Book(models.Model):
+    """
+    Represents a book written by an author.
+    Includes title, publication year, and a foreign key to Author.
+    """
+    title = models.CharField(max_length=200)
+    publication_year = models.IntegerField()
+    author = models.ForeignKey(
+        Author,
+        on_delete=models.CASCADE,
+        related_name='books'
+    )
+
+    def __str__(self):
+        return f"{self.title} ({self.publication_year})"
